a9fa318c395dce69dee2434d1de5255e674749f3
KyleBD <kylebradleydyer15@outlook.com>
2024-02-18 16:38:16 -0500

    Created and plotted a monte carlo simulation for a given portfolio.

--- src/get_tickers/getTickers.py
+++ src/get_tickers/getTickers.py
@@ -7,8 +7,14 @@ import pandas as pd
 import yfinance as yf
 yf.pdr_override()
 
-
+# ==============Define global variables============
+MonteCarloSims = 100
 stockList = ['SU', 'T']
+simTimeFrame = 100
+initialPortfolioVal = 10000 #Initial value in dollars.
+#==================================================
+
+#Chose stocks on the TSX
 stocks = [stock +'.TO' for stock in stockList]
 
 
@@ -28,12 +34,33 @@ def portfolioPerformance(weights, meanReturns, covMatrix, Time):
 
 
 endDate = dt.datetime.now()
-startDate = endDate - dt.timedelta(days=800)
+startDate = endDate - dt.timedelta(days=300)
 
 returns, meanReturns, covMatrix = getData(stocks, start=startDate, end=endDate)
 returns = returns.dropna()
+weights = np.random.random(len(meanReturns))
 
-weights = np.random.random(len(returns.columns))
+#Normalize the weights matrix
 weights /= np.sum(weights)
 
-returns['portfolio'] = returns.dot(weights)
+#Creating the Monte Carlo Simulation
+
+#Define the returns matrix dimensions based on num of stocks and simulation time.
+meanReturnsMatrix = np.full(shape=(simTimeFrame, len(weights)), fill_value=meanReturns)
+meanReturnsMatrix = meanReturnsMatrix.T
+
+simulatedReturns = np.full(shape=(simTimeFrame, MonteCarloSims), fill_value=0.0)
+
+#Use formula for computing daily returns (Represented by the covarients matrix)
+for sim in range(0, MonteCarloSims):
+    randVar = np.random.normal(size=(simTimeFrame, len(weights)))
+    #Formula uses Cholesky Decomp to find the lower triangular, tying together the multiple variables.
+    lowerTriangular = np.linalg.cholesky(covMatrix)
+    dailyReturns = meanReturnsMatrix + np.inner(lowerTriangular, randVar)
+    simulatedReturns[:,sim] = np.cumprod(np.inner(weights, dailyReturns.T)+1) * initialPortfolioVal
+
+plt.plot(simulatedReturns)
+plt.xlabel('Time (Days)')
+plt.ylabel('Portfolio Value (CAD)')
+plt.title('Monte Carlo sim of given portfolio')
+plt.show()
