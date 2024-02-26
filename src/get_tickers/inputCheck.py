4cf29794b5727a5986418ee4f1ff16605e042b4c
KyleBD <kylebradleydyer15@outlook.com>
2024-02-25 19:23:52 -0500

    Small change made so that the function will check an entire portfolio at once instead of one stock.

--- src/get_tickers/inputCheck.py
+++ src/get_tickers/inputCheck.py
@@ -3,10 +3,12 @@ from pandas_datareader import data as pdr
 
 yf.pdr_override()
 
-def check_valid_stock(ticker) -> bool:
-    data = pdr.get_data_yahoo(ticker)
-    check_existance = data.index.values.size
-    if(check_existance == 0):
-        return False
-    else:
-        return True
+def check_valid_stock(portfolio) -> bool:
+    for stock in portfolio:
+        data = pdr.get_data_yahoo(stock)
+        check_existance = data.index.values.size
+        if(check_existance == 0):
+            return False
+
+    return True
+
